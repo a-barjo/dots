76da764e339ced92e2413b5cd812f4f86520f35f
Adam <adam@barjo.io>
2026-04-13 02:15:00 +0100

    updated

--- nvim/plugin/jellybeans.lua
+++ nvim/plugin/jellybeans.lua
@@ -2,11 +2,38 @@ vim.pack.add({ "https://github.com/nanotech/jellybeans.vim" })
 
 vim.cmd.colorscheme("jellybeans")
 
-vim.api.nvim_set_hl(0, "EndOfBuffer", { link = "Comment" })
-vim.api.nvim_set_hl(0, "FloatBorder", { link = "NonText" })
-vim.api.nvim_set_hl(0, "Normal", { fg = "White" })
-vim.api.nvim_set_hl(0, "NormalFloat", {})
-vim.api.nvim_set_hl(0, "SignColumn", { link = "LineNr" })
+local c = {
+  "#0c0c12",
+  "#17161f",
+  "#262333",
+  "#5c587d",
+}
+
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "White", bg = c[3] })
 vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { undercurl = true, sp = "Red" })
 vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { undercurl = true, sp = "LightBlue" })
 vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { undercurl = true, sp = "Yellow" })
+vim.api.nvim_set_hl(0, "DiffAdd", { bg = c[3] })
+vim.api.nvim_set_hl(0, "DiffChange", { bg = c[3] })
+vim.api.nvim_set_hl(0, "DiffDelete", { fg = c[3] })
+vim.api.nvim_set_hl(0, "DiffText", { bg = c[4] })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { link = "NonText" })
+vim.api.nvim_set_hl(0, "FloatBorder", { link = "NonText" })
+vim.api.nvim_set_hl(0, "FoldColumn", { link = "LineNr" })
+vim.api.nvim_set_hl(0, "Folded", { link = "LineNr" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = c[4], bg = c[2] })
+vim.api.nvim_set_hl(0, "NonText", { fg = c[3], italic = true })
+vim.api.nvim_set_hl(0, "Normal", { fg = "White" })
+vim.api.nvim_set_hl(0, "NormalFloat", {})
+vim.api.nvim_set_hl(0, "Pmenu", { link = "LineNr" })
+vim.api.nvim_set_hl(0, "PmenuMatch", { fg = "White" })
+vim.api.nvim_set_hl(0, "PmenuSel", { bg = c[3], fg = c[4] })
+vim.api.nvim_set_hl(0, "SignColumn", { link = "LineNr" })
+vim.api.nvim_set_hl(0, "StatusLine", { link = "CursorLineNr" })
+vim.api.nvim_set_hl(0, "StatusLineNC", { link = "LineNr" })
+vim.api.nvim_set_hl(0, "TabLine", { link = "LineNr" })
+vim.api.nvim_set_hl(0, "TabLineFill", { link = "TabLine" })
+vim.api.nvim_set_hl(0, "TabLineSel", { link = "CursorLineNr" })
+vim.api.nvim_set_hl(0, "Visual", { bg = c[3] })
+vim.api.nvim_set_hl(0, "WinSeparator", { link = "LineNr" })
+vim.api.nvim_set_hl(0, "netrwMarkFile", { link = "CurSearch" })
--- nvim/plugin/tv.lua
+++ nvim/plugin/tv.lua
@@ -1,3 +1,5 @@
 vim.pack.add({ "https://github.com/alexpasmantier/tv.nvim" })
 
 vim.keymap.set("n", "<leader><leader>", "<Cmd>Tv files<CR>", { desc = "Search files" })
+vim.keymap.set("n", "<leader>d", "<Cmd>Tv dirs<CR>", { desc = "Search directories" })
+vim.keymap.set("n", "<leader>t", "<Cmd>Tv text<CR>", { desc = "Search text" })
